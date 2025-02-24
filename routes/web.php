10b4be189e3131024341e74d3a8716eaa3c6b4e7
Mansour  Al-Dadah <mansouraldadah@gmail.com>
2025-02-24 20:48:20 +0200

    Fourth Task

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -29,32 +31,18 @@ Route::post('/about', function () {
     return view('about', compact('name', 'departments'));
 });
 
-Route::get('tasks', function () {
-    $tasks = DB::table('tasks')->get();
-    return view('tasks', compact('tasks'));
-});
-Route::post('create', function () {
-    $name = $_POST['name'];
+Route::get('tasks', [TaskController::class, 'index']);
+Route::post('create', [TaskController::class, 'create']);
+Route::post('delete/{id}', [TaskController::class, 'destroy']);
+Route::post('edit/{id}', [TaskController::class, 'edit']);
+Route::post('update', [TaskController::class, 'update']);
 
-    DB::table('tasks')->insert(['name' => $name]);
+Route::get('users', [UserController::class, 'index']);
+Route::post('create', [UserController::class, 'create']);
+Route::post('delete/{id}', [UserController::class, 'destroy']);
+Route::post('edit/{id}', [UserController::class, 'edit']);
+Route::post('update', [UserController::class, 'update']);
 
-    return redirect()->back();
-    // return view('tasks');
-});
-
-Route::post('delete/{id}', function ($id) {
-    DB::table('tasks')->where('id', $id)->delete();
-    return redirect()->back();
-});
-Route::post('edit/{id}', function ($id) {
-    $task = DB::table('tasks')->where('id', $id)->first();
-    $tasks = DB::table('tasks')->get();
-
-    return view('tasks', compact('task', 'tasks'));
-});
-Route::post('update', function () {
-    $id = $_POST['id'];
-    DB::table('tasks')->where('id', $id)->update(['name' => $_POST['name']]);
-
-    return redirect('tasks');
+Route::get('app', function () {
+    return view('layouts.app');
 });
